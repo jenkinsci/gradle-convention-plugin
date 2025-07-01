56ccfbfd7587b17a9911629088945a953c53fd7a
Aarav Mahajan <aaravmahajan2003@gmail.com>
2025-07-01 21:25:38 +0530

    refactor: Upgrade BOM management and extend dependency configuration

--- convention-plugin/src/main/kotlin/constants/PluginMetadata.kt
+++ convention-plugin/src/main/kotlin/constants/PluginMetadata.kt
@@ -7,5 +7,5 @@ public object PluginMetadata {
     public const val DISPLAY_NAME: String = "Jenkins Gradle Convention Plugin"
     public const val VERSION: String = "1.0.0"
     public const val TASK_GROUP: String = "Jenkins"
-    public const val MIN_GRADLE_VERSION: String = "8.14.2"
+    public const val MIN_GRADLE_VERSION: String = "9.0"
 }
