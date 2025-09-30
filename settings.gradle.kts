f9a6478e6467a78e0f3fd9c49f387e03ed914073
Aarav Mahajan <aaravmahajan2003@gmail.com>
2025-09-30 09:09:00 +0530

    feat(git-hooks): add pre-commit hook for conventional commits

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,23 @@
  * or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
+import org.gradle.internal.impldep.org.eclipse.jgit.hooks.Hooks.commitMsg
+
+/*
+ * Copyright 2025 Aarav Mahajan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
 pluginManagement {
     includeBuild("build-logic")
     repositories {
@@ -22,6 +39,12 @@ pluginManagement {
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.1.2"
+}
+
+gitHooks {
+    commitMsg { conventionalCommits() }
+    createHooks()
 }
 
 @Suppress("UnstableApiUsage")
