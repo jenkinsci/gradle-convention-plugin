a3077a089c47276f996134ac2c6d792405c56b29
Aarav Mahajan <aaravmahajan2003@gmail.com>
2025-09-30 08:50:24 +0530

    feat: add gradle pre-commit git hooks plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,6 +22,14 @@ pluginManagement {
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.1.2"
+}
+
+gitHooks {
+    commitMsg {
+        conventionalCommits()
+    }
+    createHooks()
 }
 
 @Suppress("UnstableApiUsage")
